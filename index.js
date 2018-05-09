a48e630f7f3d6e969b71596a4189572e70b9af6d
Nate <ndsunc@gmail.com>
2018-05-09 12:50:16 -0700

    index.js appends new tasks to new line of list.txt; can show existing To Do List; can remove from list

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const fs = require('fs')
 const path = require('path')
 const command = process.argv[2]
-
+const [ nodePath, filePath, ...arguments ] = process.argv
 const list = path.join(__dirname, 'list.txt')
+let listString = fs.readFileSync(list, 'utf-8')
+
 if (command) {
-  fs.writeFileSync(list, command)
+  for (let i = 0; i < arguments.length; i++) {
+    const dataString = listString
+
+    if (command === 'show') {
+      if (dataString.includes('\n')) {
+        let filteredData = dataString.split('\n').filter(function(n) { return n !== '' })
+
+        for (let k = 0; k < filteredData.length; k++) {
+          if (k === 0) {
+            console.log(filteredData[k]);
+          } else {
+            console.log(`${k}. ${filteredData[k]}`);
+          }
+        }
+      }
+    } else if (command.match(/remove \d/) !== null) {
+      const indexToRemove = parseInt(command[command.length - 1], 10)
+      let filteredData = dataString.split('\n').filter(function(n) { return n !== '' })
+      listString = ''
+
+      for (let k = 0; k < filteredData.length; k++) {
+        if (indexToRemove !== k) { listString = listString + filteredData[k] + '\n' }
+      }
+    } else {
+      listString = listString + arguments[i] + '\n'
+    }
+  }
+
+  fs.writeFileSync(list, listString)
 }
